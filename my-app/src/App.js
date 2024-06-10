8fa86e7c9249d170b1b0c124a7e76e6ba9619b63
DavidDock <ddocksey@live.co.uk>
2024-06-10 22:10:26 +0000

    Add router with depreciated render

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import styles from "./App.module.css";
 
+import { Route, Switch } from "react-router-dom";
+
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 
@@ -11,7 +13,16 @@ function App() {
       <div className={`d-flex flex-column flex-sm-row ${styles.Bod}`}>
         <NavBar></NavBar>
         <Container>
-          <p>Hello</p>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <p>About</p>
+            )}
+          />
+          <Route render={() => <p>Not found</p>} />
+        </Switch>
         </Container>
       </div>
     </div>
