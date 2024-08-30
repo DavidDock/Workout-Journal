0c61c05dc24cd898244d8565ebb84c9f9bf66da5
David Docksey <ddocksey@live.co.uk>
2024-08-30 15:29:31 +0000

    add workouts

--- my-app/src/pages/workouts/august/August.js
+++ my-app/src/pages/workouts/august/August.js
@@ -13,11 +13,19 @@ import Workout35 from './Workout35'
 import Workout36 from './Workout36'
 import Workout37 from './Workout37'
 import Workout38 from './Workout38'
+import Workout39 from './Workout39'
+import Workout40 from './Workout40'
+import Workout41 from './Workout41'
+import Workout42 from './Workout42'
 
 const August = () => {
 
     return (
         <div>
+            <Workout42 />
+            <Workout41 />
+            <Workout40 />
+            <Workout39 />
             <Workout38 />
             <Workout37 />
             <Workout36 />
---
+++ my-app/src/pages/workouts/august/Workout41.js
@@ -0,0 +1,52 @@
+import {React, useState} from 'react'
+
+import styles from "../../../styles/Workouts.module.css";
+import Carousel from 'react-bootstrap/Carousel';
+
+import grey from "../../../assets/greyexercise.svg";
+
+const Workout41 = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+
+        <Carousel activeIndex={index} onSelect={handleSelect} interval={null} className={styles.CarouselMain}>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3 className='mb-4'>Date: Thurs 29.08</h3>
+                    <h3 className="mb-4">Body Parts: Chest</h3>
+                    <h3 className="mb-4">Duration: Throughout the day</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3>Workout</h3>
+                    <h4>Press-up Challenge </h4>
+                    <p>
+                        400 <a href='https://en.wikipedia.org/wiki/Push-up' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for press-ups (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>Press-Ups
+                            </a> throughout the day.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+
+    )
+}
+
+export default Workout41
