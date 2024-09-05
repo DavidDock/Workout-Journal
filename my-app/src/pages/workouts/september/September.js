15b9ff0a6214bda144cf5bee4f321af8b3f47f76
David Docksey <ddocksey@live.co.uk>
2024-09-05 07:31:40 +0000

    add workouts

--- my-app/src/pages/workouts/september/September.js
+++ my-app/src/pages/workouts/september/September.js
@@ -3,11 +3,13 @@ import Workout43 from './Workout43'
 import Workout44 from './Workout44'
 import Workout45 from './Workout45'
 import Workout46 from './Workout46'
+import Workout47 from './Workout47'
 
 const September = () => {
 
     return (
         <div>
+            <Workout47 />
             <Workout46 />
             <Workout45 />
             <Workout44 />
