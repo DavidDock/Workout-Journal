6993abd3eb7e60f65f384c7135484d80ca0107d0
David Docksey <ddocksey@live.co.uk>
2024-10-09 07:42:34 +0000

    add workout

--- my-app/src/pages/workouts/october/October.js
+++ my-app/src/pages/workouts/october/October.js
@@ -3,11 +3,13 @@ import Workout64 from './Workout64'
 import Workout65 from './Workout65'
 import Workout66 from './Workout66'
 import Workout67 from './Workout67'
+import Workout68 from './Workout68'
 
 const October = () => {
 
     return (
         <div>
+            <Workout68 />
             <Workout67 />
             <Workout66 />
             <Workout65 />
---
+++ my-app/src/pages/workouts/october/Workout68.js
@@ -0,0 +1,73 @@
+import {React, useState} from 'react'
+
+import styles from "../../../styles/Workouts.module.css";
+import Carousel from 'react-bootstrap/Carousel';
+
+import grey from "../../../assets/greyexercise.svg";
+
+const Workout68 = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+
+        <Carousel activeIndex={index} onSelect={handleSelect} interval={null} className={styles.CarouselMain}>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3 className='mb-4'>Date: Weds 09.10</h3>
+                    <h3 className="mb-4">Body Parts: Legs</h3>
+                    <h3 className="mb-4">Duration: 30 minutes</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3 className='mb-4'>Workout</h3>
+                    <ul>
+                        <li>
+                            <a href='https://en.wikipedia.org/wiki/Squat_(exercise)' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for squats (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>100 - Air Squats
+                            </a>
+                        </li>
+                        <li>
+                            1 Mile Run
+                        </li>
+                        <li>
+                            <a href='https://en.wikipedia.org/wiki/Squat_(exercise)' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for squats (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>100 - Air Squats
+                            </a>
+                        </li>                        
+                        <li>
+                            <a href='https://en.wikipedia.org/wiki/Calf_raises' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for Calve Raises (opens in a new tab)" className='text-white text-decoration-underline text-decoration-underline link-secondary'>100 - Standing Calve Raises
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://musclewiki.com/Bodyweight/Male/Quads/Bulgarian-Split-Squat' target="_blank" rel="noreferrer"
+                                aria-label="Website demonstrating Bulgarian split squat (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>50 Bulgarian Split Squats each leg
+                            </a>
+                        </li>
+                        <li>
+                            1 Mile Run
+                        </li>
+                    </ul>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+
+    )
+}
+
+export default Workout68
