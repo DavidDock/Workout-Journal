8e3b538ba0c6b7073f0acbebc1c70fdbe260db70
David Docksey <ddocksey@live.co.uk>
2024-10-11 10:44:55 +0000

    adjust welcome message

--- my-app/src/components/Welcome.js
+++ my-app/src/components/Welcome.js
@@ -7,7 +7,7 @@ import grey from "../../src/assets/greyexercise.svg";
 const Welcome = () => {
     return (
         <div className={styles.Main}>
-            <h2 className={styles.WelcomeStay}>STAY</h2>
+            <h2 className={styles.WelcomeStay}>STAYING</h2>
             <img
                 src={grey}
                 alt="background"
