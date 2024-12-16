cf99a7c9f54a5a2270faeb8592ddadb0637bc8ed
David Docksey <ddocksey@live.co.uk>
2024-12-16 08:43:09 +0000

    add workouts

--- my-app/src/pages/workouts/december/December.js
+++ my-app/src/pages/workouts/december/December.js
@@ -6,11 +6,17 @@ import Workout104 from './Workout104'
 import Workout105 from './Workout105'
 import Workout106 from './Workout106'
 import Workout107 from './Workout107'
+import Workout108 from './Workout108'
+import Workout109 from './Workout109'
+import Workout110 from './Workout110'
 
 const December = () => {
 
     return (
         <div>
+            <Workout110 />
+            <Workout109 />
+            <Workout108 />
             <Workout107 />
             <Workout106 />
             <Workout105 />
---
+++ my-app/src/pages/workouts/december/Workout108.js
@@ -0,0 +1,81 @@
+import {React, useState} from 'react'
+
+import styles from "../../../styles/Workouts.module.css";
+import Carousel from 'react-bootstrap/Carousel';
+
+import grey from "../../../assets/greyexercise.svg";
+
+const Workout108 = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+
+        <Carousel activeIndex={index} onSelect={handleSelect} interval={null} className={styles.CarouselMain}>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3 className='mb-4'>Date: Mon 16.12</h3>
+                    <h3 className="mb-4">Body Parts: Back and Biceps</h3>
+                    <h3 className="mb-4">Duration: 15 minutes</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3 className='mb-4'>Exercises:</h3>
+                    <ul>
+                        <li>
+                            <a href='https://en.wikipedia.org/wiki/Pull-up_(exercise)' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for pull-ups (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>Pull-Ups
+                            </a>
+                        </li>
+                        
+                        <li>
+                            <a href='https://en.wiktionary.org/wiki/chin-up' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for Chin-ups (opens in a new tab)" className='text-white text-decoration-underline text-decoration-underline link-secondary'>Chin-ups
+                            </a>
+                        </li>
+                        
+                    </ul>
+                    <h3 className='mb-4'>Equipment:</h3>
+                    <ul>
+                        <li>Pull-Up bar</li>
+                        
+                    </ul>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3>Workout</h3>
+                    <h4>Pull-up and chin-up challenge </h4>
+                    <ul>
+                        <li>Max pull-ups, 30 seconds break</li>
+                        <li>Repeat until 150 reps of pull ups complete</li>
+                        <li>Max Chin-ups, rest 30 seconds</li>
+                        <li>Repeat until 100 reps of chin ups complete</li>
+                    </ul>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+
+    )
+}
+
+export default Workout108
