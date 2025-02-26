3c056cf9082b7564dabfdaa3c9bff95b8ff5e75d
DavidDock <ddocksey@live.co.uk>
2025-02-26 09:06:11 +0000

    add workout

--- my-app/src/pages/workouts/february/February.js
+++ my-app/src/pages/workouts/february/February.js
@@ -8,11 +8,19 @@ import Workout138 from './Workout138'
 import Workout139 from './Workout139'
 import Workout140 from './Workout140'
 import Workout141 from './Workout141'
+import Workout142 from './Workout142'
+import Workout143 from './Workout143'
+import Workout144 from './Workout144'
+import Workout145 from './Workout145'
 
 const February = () => {
 
     return (
         <div>
+            <Workout145 />
+            <Workout144 />
+            <Workout143 />
+            <Workout142 />
             <Workout141 />
             <Workout140 />
             <Workout139 />
---
+++ my-app/src/pages/workouts/february/Workout145.js
@@ -0,0 +1,82 @@
+import {React, useState} from 'react'
+
+import styles from "../../../styles/Workouts.module.css";
+import Carousel from 'react-bootstrap/Carousel';
+
+import grey from "../../../assets/greyexercise.svg";
+
+const Workout145 = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+
+        <Carousel activeIndex={index} onSelect={handleSelect} interval={null} className={styles.CarouselMain}>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3 className='mb-4'>Date: Tues 25.02</h3>
+                    <h3 className="mb-4">Body Parts: Upper Body</h3>
+                    <h3 className="mb-4">Duration: 9 minutes</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3 className='mb-4'>Exercises:</h3>
+                    <ul>
+                        <li>
+                            <a href='https://en.wikipedia.org/wiki/Pull-up_(exercise)' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for pull-ups (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>Pull-Ups
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://en.wikipedia.org/wiki/Push-up' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for press-ups (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>Press-Ups
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://en.wiktionary.org/wiki/chin-up' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for Chin-ups (opens in a new tab)" className='text-white text-decoration-underline text-decoration-underline link-secondary'>Chin-ups
+                            </a>
+                        </li>
+                    </ul>
+                    <h3 className='mb-4'>Equipment:</h3>
+                    <ul>
+                        <li>Pull-up bar</li>
+                    </ul>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3>Workout</h3>
+                    <h4>3 rounds of maximum reps, 1 minute break after each round.  </h4>
+                    <ul>
+                        <li>Pull-ups</li>
+                        <li>Press-ups</li>
+                        <li>Chin-ups</li>
+                    </ul>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+
+    )
+}
+
+export default Workout145
