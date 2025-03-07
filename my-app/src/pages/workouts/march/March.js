9be69eb5f7f468570049c7a1fbcd4890a0e3b538
DavidDock <ddocksey@live.co.uk>
2025-03-07 13:20:32 +0000

    add workouts

--- my-app/src/pages/workouts/march/March.js
+++ my-app/src/pages/workouts/march/March.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Workout148 from './Workout148'
 import Workout149 from './Workout149'
+import Workout150 from './Workout150'
+import Workout151 from './Workout151'
+import Workout152 from '../january/Workout152'
 
 const March = () => {
 
     return (
         <div>
+            <Workout152 />
+            <Workout151 />
+            <Workout150 />
             <Workout149 />
             <Workout148 />       
         </div>
---
+++ my-app/src/pages/workouts/march/Workout151.js
@@ -0,0 +1,100 @@
+import {React, useState} from 'react'
+
+import styles from "../../../styles/Workouts.module.css";
+import Carousel from 'react-bootstrap/Carousel';
+
+import grey from "../../../assets/greyexercise.svg";
+
+const Workout151 = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+
+        <Carousel activeIndex={index} onSelect={handleSelect} interval={null} className={styles.CarouselMain}>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3 className='mb-4'>Date: Thur 06.03</h3>
+                    <h3 className="mb-4">Body Parts: Upper Body</h3>
+                    <h3 className="mb-4">Duration: 12 minutes</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3 className='mb-4'>Exercises:</h3>
+                    <ul>
+                        <li>
+                            <a href='https://en.wikipedia.org/wiki/Pull-up_(exercise)' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for pull-ups (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>Pull-Ups
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://en.wikipedia.org/wiki/Push-up' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for press-ups (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>Press-Ups
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://en.wiktionary.org/wiki/chin-up' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for Chin-ups (opens in a new tab)" className='text-white text-decoration-underline text-decoration-underline link-secondary'>Chin-ups
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://musclewiki.com/bodyweight/male/triceps/bench-dips' target="_blank" rel="noreferrer"
+                                aria-label="Website demonstration for tricep dips (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>Tricep Dips
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://www.wikihow.com/Do-Pike-Push-Ups' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for pike press-ups (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>Pike press-Ups
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://musclewiki.com/bodyweight/female/chest/decline-push-up' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for decline push-ups (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>Decline push-ups
+                            </a>
+                        </li>
+                    </ul>
+                    <h3 className='mb-4'>Equipment:</h3>
+                    <ul>
+                        <li>Pull-up bar</li>
+                    </ul>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3>Workout</h3>
+                    <p>2 rounds of the below with a minute rest betwwen each round</p>
+                    <ul>
+                        <li>10 Pull-ups</li>
+                        <li>20 Pres-ups</li>
+                        <li>10 Chin-ups</li>
+                        <li>20 Tricep Dips</li>
+                        <li>10 Pike Press-Ups</li>
+                        <li>20 Incline Press-Ups</li>
+                    </ul>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+
+    )
+}
+
+export default Workout151
