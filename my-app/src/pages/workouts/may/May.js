3a4fdf05cc7a1fbf4d84f8a6e392a6e1bc7fc952
DavidDock <ddocksey@live.co.uk>
2025-05-14 08:45:14 +0000

    add workouts

--- my-app/src/pages/workouts/may/May.js
+++ my-app/src/pages/workouts/may/May.js
@@ -4,11 +4,17 @@ import Workout188 from './Workout188'
 import Workout189 from './Workout189'
 import Workout190 from './Workout190'
 import Workout191 from './Workout191'
+import Workout192 from './Workout192'
+import Workout193 from './Workout193'
+import Workout194 from './Workout194'
 
 const May = () => {
 
     return (
         <div>
+            <Workout194 />
+            <Workout193 />
+            <Workout192 />
             <Workout191 />
             <Workout190 />
             <Workout189 />
---
+++ my-app/src/pages/workouts/may/Workout192.js
@@ -0,0 +1,59 @@
+import {React, useState} from 'react'
+
+import styles from "../../../styles/Workouts.module.css";
+import Carousel from 'react-bootstrap/Carousel';
+
+import grey from "../../../assets/greyexercise.svg";
+
+const Workout192 = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+
+        <Carousel activeIndex={index} onSelect={handleSelect} interval={null} className={styles.CarouselMain}>
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3 className='mb-4'>Date: Wed 14.05</h3>
+                    <h3 className="mb-4">Body Parts: Shoulders and Biceps</h3>
+                    <h3 className="mb-4">Duration: Throughout the day</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item className={styles.CarouselItem}>
+                <img
+                    src={grey}
+                    alt="background"
+                    className={styles.CarouselImage}>
+                </img>
+                <Carousel.Caption className={`my-auto ${styles.CarouselContent}`}>
+                    <h3>Press and Curl Challenge</h3>
+                    <ul>
+                        <li>
+                            150 each arm &nbsp;<a href='https://musclewiki.com/dumbbells/male/shoulders/dumbbell-single-arm-overhead-press/' target="_blank" rel="noreferrer"
+                                aria-label="Website demonstrating One arm dumbbell shoulder press (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>One arm dumbell shoulder press
+                            </a>
+                        </li>
+                        <li>
+                            150 each arm &nbsp;<a href='https://en.wikipedia.org/wiki/Bicep_curl' target="_blank" rel="noreferrer"
+                                aria-label="Wiki for Bicep curl (opens in a new tab)" className='text-white text-decoration-underline link-secondary'>One arm dumbbell bicep curls
+                            </a>
+                        </li>
+                    </ul>
+                    <p>Reps spread throughout the day as and when I had time with a 12.5 KG dumbell.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+
+    )
+}
+
+export default Workout192
