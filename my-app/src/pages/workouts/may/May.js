e726f44917604b2796e93d6d02abb16aadf8161e
DavidDock <ddocksey@live.co.uk>
2025-05-30 15:59:48 +0000

    add workouts

--- my-app/src/pages/workouts/may/May.js
+++ my-app/src/pages/workouts/may/May.js
@@ -7,11 +7,25 @@ import Workout191 from './Workout191'
 import Workout192 from './Workout192'
 import Workout193 from './Workout193'
 import Workout194 from './Workout194'
+import Workout195 from './Workout195'
+import Workout196 from './Workout196'
+import Workout197 from './Workout197'
+import Workout198 from './Workout198'
+import Workout199 from './Workout199'
+import Workout200 from './Workout200'
+import Workout201 from './Workout201'
 
 const May = () => {
 
     return (
         <div>
+            <Workout201 />
+            <Workout200 />
+            <Workout199 />
+            <Workout198 />
+            <Workout197 />
+            <Workout196 />
+            <Workout195 />
             <Workout194 />
             <Workout193 />
             <Workout192 />
